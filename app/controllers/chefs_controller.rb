796433d53d6db4cd67d92592a320a7c707eb3cfd
Alvin Ng <alvindng@gmail.com>
2016-09-12 18:08:15 -0700

    add chef edit and update  actions and view

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -11,4 +11,29 @@ class ChefsController < ApplicationController
     @chef = Chef.new
     render :new
   end
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      redirect_to chefs_path
+    else
+      render :new
+    end
+  end
+  def edit
+    @chef = Chef.find(params[:id])
+    render :edit
+  end
+  def update
+    @chef= Chef.find(params[:id])
+    if @chef.update(chef_params)
+      redirect_to chefs_path
+    else
+      render :edit
+    end
+  end
+
+private
+def chef_params
+  params.require(:chef).permit(:name, :phone, :address)
+end
 end
