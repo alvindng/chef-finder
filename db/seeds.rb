d889ed233453f1d95627ccff09c5f906743a4ed2
Alvin Ng <alvindng@gmail.com>
2016-09-23 09:21:16 -0700

    add seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_list = [
+  [ "gary@test.com", "Gary", "312-312-1321", "123 Fake St", "welcome" ],
+  [ "lucy@test.com", "Lucy", "312-312-2321", "1234 Fake St", "welcome" ],
+  [ "ricky@test.com", "Ricky", "312-312-3321", "1235 Fake St", "welcome" ],
+  [ "barry@test.com", "Barry", "312-312-4321", "1236 Fake St", "welcome" ],
+  [ "larry@test.com", "Larry", "312-312-5321", "1237 Fake St", "welcome" ],
+]
+
+user_list.each do |email, name, phone, address, password|
+  User.create( email: email, name: name, phone: phone, address: address, password: password)
+end
