f0e525ac97fb13f583b412d5765d6ca72bc0172b
Alvin Ng <alvindng@gmail.com>
2016-11-06 11:11:27 -0800

    make sign out function work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     @user = current_user
     @profile = Profile.user.find(@user)
   end
-  
+
   def after_sign_in_path_for(resource)
     if user_signed_in? &&current_user.profile
       user_profile_path(current_user, current_user.profile)
@@ -13,14 +13,15 @@ class ApplicationController < ActionController::Base
       new_user_profile_path(resource)
     end
   end
+
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
 
-protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit :email, :password, :password_confirmation
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit :email, :password, :password_confirmation
+      end
     end
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to root_path
+    root_path
   end
 
   private
